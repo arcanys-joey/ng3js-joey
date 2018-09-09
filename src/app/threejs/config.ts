cc19e599752cabe1d0ca4db81180a710dbff676c
Joey Resuento <ARCANYS\jresuento@resuento-pc.arcanys.com>
2018-09-09 20:53:43 +0800

    Revise config as interface

---
+++ src/app/threejs/config.ts
@@ -0,0 +1,11 @@
+export interface Config {
+  antialiasing?: boolean;
+  fov?: Number;
+  aspectRatio?: number;
+  near?: number;
+  far?: number;
+  enableOrbitControls?: boolean;
+  enableAxisHelper?: boolean;
+  axesSize?: number;
+  cameraZPos?: number;
+}
